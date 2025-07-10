6cfd6ae55de41b260a3f8a7fc4ceda8b2fe55461
Swyamsharma <swyamsharma13102003@gmail.com>
2025-07-10 20:56:05 +0530

    feat: Add React and ReactDOM scripts to index.html for component rendering

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({jsxRuntime: 'classic'})],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
       },
     },
   },
